cc7cba8b382ce843e9a3feddf06afa548e492478
pulakkar333 <pulakkaar@gmail.com>
2026-05-30 14:31:41 +0600

    worked in blaade

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -8,7 +8,6 @@
             <div class="row">
                 <div class="col-md-12">
                     <h2>Edit Category</h2>
-
                 </div>
             </div>
             <!-- /. ROW  -->
