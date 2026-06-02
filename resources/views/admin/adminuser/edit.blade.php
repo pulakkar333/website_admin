14ae4525315516624f19dd86098eb0846d40659c
pulakkar333 <pulakkaar@gmail.com>
2026-06-02 23:56:53 +0600

    worked in blaade

--- resources/views/admin/adminuser/edit.blade.php
+++ resources/views/admin/adminuser/edit.blade.php
@@ -8,7 +8,6 @@
             <div class="row">
                 <div class="col-md-12">
                     <h2>Edit Member Permission</h2>
-
                 </div>
             </div>
             <!-- /. ROW  -->
